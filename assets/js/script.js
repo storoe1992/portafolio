8c5496c1d7fecfe0be7a20bc2beaadcb323a9177
Sergio Toro Escofet <storoe1992@gmail.com>
2020-11-21 01:47:50 -0300

    Página terminada - Revisando especificaciones del desafío

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    $("a").click(function(e){
+        if(this.hash !== ""){
+            e.preventDefault();
+            var gato = this.hash;
+            $('html, body').animate({
+                scrollTop : $(gato).offset().top
+            },800,function(){
+                window.location.hash = gato;
+            });
+        }
+    });
+    $("[data-bs-tooltip]").tooltip();
+});
